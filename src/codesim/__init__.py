b8e7bda475852372613c1231a6e41b11cfbdd439
StardustDL <stardustdl@163.com>
2021-12-07 14:49:39 +0800

    remove unused ci env

--- src/codesim/__init__.py
+++ src/codesim/__init__.py
@@ -8,3 +8,10 @@ __version__ = "0.0.0"
 
 def get_app_directory():
     return pathlib.Path(__file__).parent
+
+
+def get_temp_directory():
+    result = get_app_directory().joinpath("temp")
+    if not result.exists() or not result.is_dir():
+        os.makedirs(result)
+    return result
---
+++ src/codesim/langs/cpp/compiler.py
@@ -0,0 +1,5 @@
+import subprocess
+from tempfile import TemporaryDirectory
+
+def compile(src):
+    pass
