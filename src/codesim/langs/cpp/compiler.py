5b723b14dc385e9cc9c438eecb2f59ea4ba995be
StardustDL <stardustdl@163.com>
2021-12-07 16:39:10 +0800

    add logs in compiler

--- src/codesim/langs/cpp/compiler.py
+++ src/codesim/langs/cpp/compiler.py
@@ -13,19 +13,21 @@ def compile(src: str):
     codeFile = temp.joinpath("src.cpp")
     outFile = temp.joinpath("out.o")
     codeFile.write_text(src, encoding="utf-8")
-    logger.info("Compiling")
     result = subprocess.run(
         ["g++", str(codeFile.absolute()), "-O2", "-o", str(outFile.absolute())], stderr=subprocess.PIPE, stdout=subprocess.PIPE, text=True, encoding="utf-8")
+    logger.info(f"Compiled with exit code {result.returncode}.")
+    if result.returncode != 0:
+        logger.error(f"Compiled failed: {result.stderr}.")
     result.check_returncode()
     return outFile
 
 
 def objdump(objfile: pathlib.Path) -> Program:
-
-    logger.info("Dumping")
-
     result = subprocess.run(
         ["objdump", "-d", "--no-show-raw-insn", str(objfile.absolute())], stderr=subprocess.PIPE, stdout=subprocess.PIPE, text=True, encoding="utf-8")
+    logger.info(f"Dumped with exit code {result.returncode}.")
+    if result.returncode != 0:
+        logger.error(f"Dumped failed: {result.stderr}.")
     result.check_returncode()
 
     output = result.stdout.splitlines()
@@ -54,12 +56,15 @@ def objdump(objfile: pathlib.Path) -> Program:
                 terms = [s.strip() for s in l.split(":")[1].strip().split(" ") if s.strip()]
                 opcode = terms[0]
                 extra = terms[1] if len(terms) > 1 else ""
-                func.instrs.append(Instruction(opcode, extra))
+                if opcode != "(bad)":
+                    func.instrs.append(Instruction(opcode, extra))
             except Exception as ex:
                 logger.error(
                     f"Failed to analysis '{l}' of {func.name}.", exc_info=ex)
         cur = p
 
+        logger.debug(f"Function {func.name} with {len(func.instrs)} instruction(s).")
         result.funcs.append(func)
 
+    logger.debug(f"Program with {len(result.funcs)} function(s).")
     return result
--- src/codesim/langs/cpp/main.py
+++ src/codesim/langs/cpp/main.py
@@ -3,11 +3,5 @@ from .compiler import compile, objdump
 
 def measure(src1: str, src2: str) -> float:
     proj1, proj2 = [objdump(compile(s)) for s in [src1, src2]]
-
-    print("proj1")
-    proj1.view()
-
-    print("proj2")
-    proj2.view()
-
+    
     return 0.0
